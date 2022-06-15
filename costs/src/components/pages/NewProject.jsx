c5d185348006df5cbb86898a10d3ae7e9405cbcc
pedroemanuel42 <99288851+pedroemanuel42@users.noreply.github.com>
2022-06-15 18:43:53 -0300

    Forms basic

--- costs/src/components/pages/NewProject.jsx
+++ costs/src/components/pages/NewProject.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import ProjectForm from "../project/ProjectForm";
+import "./new-project.css";
 
 export default function NewProject() {
-  return <h1>New Project</h1>;
+  return (
+    <div className="new-project-container">
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm />
+    </div>
+  );
 }
