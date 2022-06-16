7700336009136702ad75f9abc7be8e7f6b52b1de
pedroemanuel42 <99288851+pedroemanuel42@users.noreply.github.com>
2022-06-16 16:24:48 -0300

    Create dashboard

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+
+import "./projects.css";
 
 export default function Projects() {
   const location = useLocation();
@@ -11,9 +16,15 @@ export default function Projects() {
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className="project-container">
+      <div className="title-container">
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto"/>
+      </div>
       {message && <Message msg={message} type="success" />}
+      <Container customClass='start'>
+        <p>projetos...</p>
+      </Container>
     </div>
   );
 }
